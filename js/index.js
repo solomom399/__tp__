2b6e2cea1c3a4c6cb7f23f9707fed844afb89d16
solomon <sobanjosodiq@gmail.com>
2017-10-02 15:35:15 -0700

    __tp__

--- js/index.js
+++ js/index.js
@@ -1,30 +1,16 @@
-/*document.addEventListener("deviceready", onDeviceReady, false);
+// document.addEventListener("deviceready", onDeviceReady, false);
 
-function onDeviceReady() {
-    // Now safe to use device APIs
-    window.PaystackPlugin.chargeCard(
-      function(resp) {
-        // charge successful, grab transaction reference - do your thang!
-        console.log('charge successful: '+JSON.stringify(resp));
-      },
-      function(resp) {
-        // Something went wrong, oops - perhaps an invalid card.
-        console.log('charge failed: '+JSON.stringify(resp));
-      },
-      {
-        cardNumber: '4084084084084081', 
-        expiryMonth: '01', 
-        expiryYear: '20', 
-        cvc: '408',
-        email: 'sobanjosodi@gmail.com',
-        amountInKobo: 150000
-    });
+$(".pay").on('submit', function () {
+  var name = $(this).find('#name').val()
+  var email = $(this).find('#email').val()
+  var amount = $(this).find('#amount').val()
+
+  pay('http://pay.freshhopesystems.com/?name='+name+'&email='+email+'&amount'+amount)
+  return false
+})
+
+function pay(url) {
+  var ref = cordova.InAppBrowser.open(url, '_blank', 'location=no');
 }
-*/
-document.addEventListener("deviceready", onDeviceReady, false);
 
 
-function onDeviceReady() {
-  var ref = cordova.InAppBrowser.open('https://eattutorial.com/portal', '_blank', 'location=no');
-
-}
